4cae1dbd2ccd2404bd4d0d43632291218f6fe2ae
nnkongh <haukong1308@gmail.com>
2026-03-30 03:51:01 +0700

    Class nau duoc them vao

--- test repo/Class1.cs
+++ test repo/Class1.cs
@@ -5,5 +5,13 @@
         public int Hi { get; set; }
         public string hehehe {get;set;}
         public int hi2 { get; set;}
+        public int hi3 { get; set;}
+        public int hi4 { get; set;}
+        public int hi5 { get; set;}
+
+    }
+    public class MyClass
+    {
+
     }
 }
